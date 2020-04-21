0946d5008fd1ffbebbca7e84fa8e4b8bc4da7076
SergiBvs <sergi.barrachina@epiagranollers.cat>
2020-04-21 17:29:00 +0200

    h

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float m_directionX;
-    float m_directionY;
-
+    Vector2 m_movement;
     public Rigidbody2D m_PlayerRB2D;
     public int m_PlayerSpeed;
 
@@ -19,42 +17,28 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-        m_directionX = Input.GetAxisRaw("Horizontal");
-        m_directionY = Input.GetAxisRaw("Vertical");
+        //MOVIMIENTO
 
-        if(m_directionX >0) //go left
-        {
-            m_PlayerRB2D.AddForce(new Vector2(1, 0) * m_PlayerSpeed, ForceMode2D.Impulse);
-        }
-        else if(m_directionX <0) //go right
-        {
-            m_PlayerRB2D.AddForce(new Vector2(-1, 0) * m_PlayerSpeed, ForceMode2D.Impulse);
-        }
+        m_movement.x = Input.GetAxisRaw("Horizontal");
+        m_movement.y = Input.GetAxisRaw("Vertical");
 
-        if(m_directionY < 0) //go down
-        {
-            m_PlayerRB2D.AddForce(new Vector2(0, -1) * m_PlayerSpeed, ForceMode2D.Impulse);
-        }
-        else if(m_directionY > 0) //go up
-        {
-            m_PlayerRB2D.AddForce(new Vector2(0, 1) * m_PlayerSpeed, ForceMode2D.Impulse);
-        }
+        m_PlayerRB2D.MovePosition(m_PlayerRB2D.position + m_movement * m_PlayerSpeed * Time.deltaTime);
 
-        if (Mathf.Abs(m_PlayerRB2D.velocity.x) >= 0 && m_directionX != 0)
-        {
-            m_PlayerRB2D.velocity = new Vector2(m_PlayerSpeed * m_directionX, m_PlayerRB2D.velocity.y);
-        }
+        //APUNTADO
 
-        if (Mathf.Abs(m_PlayerRB2D.velocity.y) >= 0 && m_directionY != 0)
-        {
-            m_PlayerRB2D.velocity = new Vector2(m_PlayerRB2D.velocity.x, m_PlayerSpeed * m_directionY);
-        }
+    }
 
-        if (m_directionX == 0 && m_directionY == 0)
+    Vector3 lookPos;
+    void RotatePlayerAlongMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
         {
-            m_PlayerRB2D.velocity = new Vector2(0, 0);
+            lookPos = hit.point;
         }
-
-        //i dont know what the fuck am i doing 
+        Vector3 lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+        transform.LookAt(transform.position + lookDir, Vector3.up);
     }
 }
