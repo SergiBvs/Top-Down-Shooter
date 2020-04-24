fdbc98050fdddbe62bf4587afa40c5cb29b6a93a
Skfod37 <alex.morillas@epiagranollers.cat>
2020-04-24 12:32:29 +0200

    ea

---
+++ Assets/Enemy_Basic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Basic : MonoBehaviour
+{
+    private GameObject m_Player;
+    public int m_Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //NOT WORKING XD
+        if (Physics2D.Raycast(transform.position, m_Player.transform.position, 5))
+        {
+            Debug.DrawRay(transform.position, m_Player.transform.position, Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, m_Player.transform.position, Color.gray);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,16 +18,14 @@ public class Player : MonoBehaviour
     void Update()
     {
         //MOVIMIENTO
-
         m_movement.x = Input.GetAxisRaw("Horizontal");
         m_movement.y = Input.GetAxisRaw("Vertical");
 
         m_PlayerRB2D.MovePosition(m_PlayerRB2D.position + m_movement * m_PlayerSpeed * Time.deltaTime);
 
+
         //APUNTADO
-
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         transform.rotation = Quaternion.LookRotation(Vector3.forward, (mousePosition - transform.position).normalized); 
     }
 
