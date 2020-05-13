37891f5a00149d29a1d2dbb5396c78382b9b66dd
SergiBvs <sergi.barrachina@epiagranollers.cat>
2020-05-13 18:22:29 +0200

    lasergun

--- Assets/Scripts/Guns/LaserGun.cs
+++ Assets/Scripts/Guns/LaserGun.cs
@@ -6,6 +6,7 @@ public class LaserGun : Gun
 {
 
     public int m_DamagePerAmmoUnit;
+    public LineRenderer m_LR;
     
     public override void  Update()
     {
@@ -13,7 +14,7 @@ public class LaserGun : Gun
 
         if (Input.GetButton("Fire1"))
         {
-            if (m_canShoot && m_HasBullets && !m_IsReloading && !GameManager.instance.m_GameIsPaused)
+            if (m_HasBullets && !m_IsReloading && !GameManager.instance.m_GameIsPaused)
                 Shoot(player.localRotation.eulerAngles.z);
         }
 
@@ -31,39 +32,79 @@ public class LaserGun : Gun
 
     public override void Shoot(float rotationZ)
     {
-        m_canShoot = false;
 
         RaycastHit2D hit = Physics2D.Raycast(GunTip.position, transform.right , 30);
         Debug.DrawRay(GunTip.position, transform.right, Color.red, hit.distance);
 
-        StartCoroutine(GunCooldown());
 
-        m_CurrentAmmo--;
-
-        //aixo hauria de funcionar no?
-
-        if (m_CurrentAmmo <= (m_Magazine / 2))
+        if(m_canShoot)
         {
-            GUIHelp.m_AmmoText.color = new Color(255, 129, 129);
-        }
+            StartCoroutine(GunCooldown());
 
-        if (m_CurrentAmmo <= (m_Magazine / 4))
-        {
-            GUIHelp.m_AmmoText.color = new Color(255, 57, 57);
-        }
+            m_CurrentAmmo--;
 
-        //aixo si funciona
+            m_canShoot = false;
+       
 
-        if (m_CurrentAmmo <= 0)
-        {
-            GUIHelp.m_ReloadPanel.SetActive(true);
-            GUIHelp.m_AmmoText.color = new Color(255, 0, 0);
-            m_HasBullets = false;
-        }
+            //aixo hauria de funcionar no?
 
-        if(hit)
-        {
-            
+            /* if (m_CurrentAmmo <= (m_Magazine / 2))
+             {
+                 GUIHelp.m_AmmoText.color = new Color(255, 129, 129);
+             }
+
+             if (m_CurrentAmmo <= (m_Magazine / 4))
+             {
+                 GUIHelp.m_AmmoText.color = new Color(255, 57, 57);
+             }*/
+
+            //aixo si funciona
+
+
+            if (m_CurrentAmmo <= 0)
+            {
+                GUIHelp.m_ReloadPanel.SetActive(true);
+                GUIHelp.m_AmmoText.color = new Color(255, 0, 0);
+                m_HasBullets = false;
+            }
+
+            if(hit)
+            {
+
+                //particulas de colision con el laser (podemos usar las del enemigo de laser maybe)
+
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    Enemy l_Enemy = hit.collider.gameObject.GetComponent<Enemy>();
+                    l_Enemy.TakeDamage(m_DamagePerAmmoUnit);
+                }
+                else if (hit.collider.CompareTag("Wall"))
+                {
+                    //particles
+                }
+                else if(hit.collider.CompareTag("Window"))
+                {
+                    //particles
+                }
+
+                m_LR.SetPosition(0, GunTip.position);
+                m_LR.SetPosition(1, hit.point);
+            }
+            else
+            {
+                m_LR.SetPosition(0, GunTip.position);
+                m_LR.SetPosition(1, GunTip.position+GunTip.right *100);
+            }
+
+            m_LR.enabled = true;
+
+            StartCoroutine(LineCD());
         }
+    }
+
+    IEnumerator LineCD()
+    {
+        yield return new WaitForSeconds(0.02f);
+        m_LR.enabled = false;
     }
 }
