4125312c4cda887e64d294e9a2ab6cd0a799adfa
SergiBvs <sergi.barrachina@epiagranollers.cat>
2020-05-14 19:09:52 +0200

    shop

---
+++ Assets/Scripts/Shop/EnterExitShop.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnterExitShop : MonoBehaviour
+{
+
+    public GameObject m_PressBtoBuyText;
+    public GameObject m_GUICanvas;
+    public GameObject m_ReloadBarCanvas;
+    public GameObject m_ShopPanel;
+
+    public bool m_IsNearShop;
+    public TextMeshProUGUI m_CurrencyText;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.B) && !GameManager.instance.m_IsInShop && m_IsNearShop)
+            EnterShop();
+        else if (Input.GetKeyDown(KeyCode.B) && GameManager.instance.m_IsInShop)
+            ExitShop();
+
+        if(GameManager.instance.m_IsInShop)
+        {
+            m_CurrencyText.text = GameManager.instance.m_Currency + "$";
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            m_PressBtoBuyText.SetActive(true);
+            m_IsNearShop = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            m_PressBtoBuyText.SetActive(false);
+            m_IsNearShop = false;
+        }
+    }
+
+    public void EnterShop()
+    {
+        m_PressBtoBuyText.SetActive(false);
+        m_GUICanvas.SetActive(false);
+        m_ReloadBarCanvas.SetActive(false);
+        Time.timeScale = 0;
+        GameManager.instance.m_IsInShop = true;
+        m_ShopPanel.SetActive(true);
+    }
+
+    public void ExitShop()
+    {
+        m_PressBtoBuyText.SetActive(true);
+        m_GUICanvas.SetActive(true);
+        Time.timeScale = 1;
+        GameManager.instance.m_IsInShop = false;
+        m_ShopPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Shop/ShopScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopScript : MonoBehaviour
+{
+
+    public int m_WeaponCost;
+    private Player m_PlayerScript;
+
+    public TextMeshProUGUI m_WeaponCostText;
+    
+    void Start()
+    {
+        m_PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        //m_WeaponCosts = new int[3];
+    }
+
+    void Update()
+    {
+        m_WeaponCostText.text = m_WeaponCost + "$";
+    }
+
+
+    public void BuyWeapons()
+    {
+        if(this.gameObject.CompareTag("AR"))
+        {
+            if(GameManager.instance.m_Currency >= m_WeaponCost && m_PlayerScript.GunBoughtArray[1] == false)
+            {
+                m_PlayerScript.GunBoughtArray[1] = true;
+                GameManager.instance.m_Currency -= m_WeaponCost;
+            }
+            else
+            {
+                //mostrar que no puedes comprar el arma o que ya la tienes
+            }
+        }
+        else if (this.gameObject.CompareTag("Shotgun"))
+        {
+            if (GameManager.instance.m_Currency >= m_WeaponCost && m_PlayerScript.GunBoughtArray[2] == false)
+            {
+                m_PlayerScript.GunBoughtArray[2] = true;
+                GameManager.instance.m_Currency -= m_WeaponCost;
+            }
+            else
+            {
+                //mostrar que no puedes comprar el arma o que ya la tienes
+            }
+        }
+        else if(this.gameObject.CompareTag("LaserGun"))
+        {
+            if (GameManager.instance.m_Currency >= m_WeaponCost && m_PlayerScript.GunBoughtArray[3] == false)
+            {
+                m_PlayerScript.GunBoughtArray[3] = true;
+                GameManager.instance.m_Currency -= m_WeaponCost;
+            }
+            else
+            {
+                //mostrar que no puedes comprar el arma o que ya la tienes
+            }
+        }
+    }
+
+    public void BuyUpgrades()
+    {
+        if (this.gameObject.CompareTag("HealthUpgrade"))
+        {
+            //max health +25 max 175
+        }
+        else if (this.gameObject.CompareTag("LuckUpgrade"))
+        {
+            //mas luck
+        }
+        else if (this.gameObject.CompareTag("AmmoUpgrade"))
+        {
+            //mas ammo total
+        }
+        else if (this.gameObject.CompareTag("AmmoRefill"))
+        {
+            if (this.gameObject.name == "AmmoSmallRefill")
+            {
+                //pequeño incremento, si tienes poco dinero
+            }
+            else if (this.gameObject.name == "AmmoFullRefill")
+            {
+                //toda la municion restaurada
+            }
+        }
+
+        //lo que sea que se quiera añadir 
+    }
+
+
+}
