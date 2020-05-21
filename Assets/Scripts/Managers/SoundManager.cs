c6197d4be60f7f84be00d59965d94fb3d1b4c4ae
SergiBvs <sergi.barrachina@epiagranollers.cat>
2020-05-21 11:54:27 +0200

    r
    r

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource m_AS;
+
+    public AudioClip[] m_AudioClips;
+  
+    public void PlaySound(string SoundClip)
+    {
+        Instantiate(m_AS);
+
+        foreach (AudioClip clip in m_AudioClips)
+        {
+            if (clip.name == SoundClip)
+            {
+                m_AS.clip = clip;
+                m_AS.Play();
+            }
+        }
+
+        /*if (cuando deje de sonar)
+            Destroy(m_AS);*/
+    }
+}
+
